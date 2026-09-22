8354d7c0c3942186c55906918ef8d3426809e4a4
neon-cs3 <flitrasyontarsus@gmail.com>
2026-09-22 13:00:27 +0300

    Update author list in build.gradle.kts

--- HDFilmCehennemi/build.gradle.kts
+++ HDFilmCehennemi/build.gradle.kts
@@ -1,7 +1,7 @@
 version = 18
 
 cloudstream {
-    authors     = listOf("hexated", "Kayracs3")
+    authors     = listOf("hexated", "neon-cs3")
     language    = "tr"
     description = "Türkiye'nin en hızlı hd film izleme sitesi"
 

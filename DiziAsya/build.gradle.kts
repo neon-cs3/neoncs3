1c1c93040f20ff632212d4bb4148967a80af53c2
neoncs3 <flitrasyontarsus@gmail.com>
2026-09-24 13:25:18 +0300

    Update authors and icon URL in build.gradle.kts

--- DiziAsya/build.gradle.kts
+++ DiziAsya/build.gradle.kts
@@ -2,11 +2,11 @@ version = 1
 
 cloudstream {
     description = "DiziPal Türkiye katalog ve bölüm sağlayıcısı"
-    authors = listOf("DiziPalCloudStream")
+    authors = listOf("DiziAsya")
     status = 1
     tvTypes = listOf("TvSeries")
     language = "tr"
-    iconUrl = "https://dizipal1583.com/favicon.ico"
+    iconUrl = "https://diziasya.com/favicon.ico"
 }
 
 android {

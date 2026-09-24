cd006f643ea8f764f31a4fbf55d5e1b24b582151
neoncs3 <flitrasyontarsus@gmail.com>
2026-09-24 15:08:38 +0300

    Add files via upload

---
+++ DiziBoxizle/src/main/kotlin/com/neoncs3/DiziBoxizlePlugin.kt
@@ -0,0 +1,12 @@
+package com.flitresyontarsus.dizipal
+
+import android.content.Context
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+
+@CloudstreamPlugin
+class DiziPalPlugin : Plugin() {
+    override fun load(context: Context) {
+        registerMainAPI(DiziPal())
+    }
+}

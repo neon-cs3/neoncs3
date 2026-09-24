b52f1eb238d8eaf6bb891f5741652444180ebb8a
neoncs3 <flitrasyontarsus@gmail.com>
2026-09-24 13:24:03 +0300

    Add files via upload

---
+++ DiziAsya/build.gradle.kts
@@ -0,0 +1,16 @@
+version = 1
+
+cloudstream {
+    description = "DiziPal Türkiye katalog ve bölüm sağlayıcısı"
+    authors = listOf("DiziPalCloudStream")
+    status = 1
+    tvTypes = listOf("TvSeries")
+    language = "tr"
+    iconUrl = "https://dizipal1583.com/favicon.ico"
+}
+
+android {
+    buildFeatures {
+        buildConfig = true
+    }
+}

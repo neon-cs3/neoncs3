53a608490ed1b7e908d2bd5afd390e53814c47c6
neoncs3 <flitrasyontarsus@gmail.com>
2026-09-24 13:55:27 +0300

    Update authors and icon URL in build.gradle.kts

--- DiziKorea/build.gradle.kts
+++ DiziKorea/build.gradle.kts
@@ -2,11 +2,11 @@ version = 1
 
 cloudstream {
     description = "DiziPal Türkiye katalog ve bölüm sağlayıcısı"
-    authors = listOf("DiziPalCloudStream")
+    authors = listOf("DiziKorea")
     status = 1
     tvTypes = listOf("TvSeries")
     language = "tr"
-    iconUrl = "https://dizipal1583.com/favicon.ico"
+    iconUrl = "https://dizikorea3.com//favicon.ico"
 }
 
 android {
